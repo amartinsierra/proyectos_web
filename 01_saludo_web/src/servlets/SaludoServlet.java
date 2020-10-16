3f4f332914f1547c8e5f2b47bfdb92ddcc4c3299
Antonio <Antonio@DESKTOP-J8ARSSP>
2020-10-16 10:21:36 +0200

    commit inicial

---
+++ 01_saludo_web/src/servlets/SaludoServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/SaludoServlet")
+public class SaludoServlet extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		response.setContentType("text/html");
+		out.println("<html><body><center>");
+		for(int i=1;i<=6;i++) {
+			out.println("<h"+i+">Bienvenido a mi servlet!</h"+i+">");
+		}
+		out.println("</center></body></html>");
+		out.close();
+	}
+
+}
